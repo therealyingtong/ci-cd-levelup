cb3a817429663d3e06b570fae218b8bb52a9b8c2
therealyingtong <yingtong@z.cash>
2020-11-11 14:27:41 +0800

    Assign dynamic port

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => {
   res.status(200).json('Hello world')
 })
 
-module.exports = app.listen(4000, () => console.log(`Running on http://localhost:4000`))
+module.exports = app.listen(process.env.PORT || 4000)
